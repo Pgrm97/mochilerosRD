32e3e7c7ba1b12bb0df1e8ec0fac28e69a113f87
Pgrm97 <Pgrm97@gmail.com>
2020-11-14 10:27:48 -0400

    Added the intro component

---
+++ components/IntroComponent.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Image, View, StyleSheet } from 'react-native'
+import { Text, Button } from 'react-native-elements'
+
+function Intro(props) {
+
+    return(
+        <View style={ styles.container }>
+            <Text style={{marginBottom: 30}} h4>Welcome to Mochileros RD!</Text>
+            <Image source={{uri: 'https://cdn0.iconfinder.com/data/icons/tutor-icon-set/512/Backpack_icon-512.png'}}
+            style={{width: 150, height: 150}} />
+            <Button
+              style={{marginTop: 50}}
+              title="Continue"
+              onPress={ () => props.navigation.navigate('Card', { name: 'Jane' })}
+              type="solid"
+            />
+        </View>
+        
+    );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default Intro;
