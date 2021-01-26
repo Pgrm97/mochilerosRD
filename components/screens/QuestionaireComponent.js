0b5661522b3af371fe09cc1c2bc4937edf1397ca
Pgrm97 <Pgrm97@gmail.com>
2021-01-26 12:44:21 -0400

    Finished Initial Form with Date.

--- components/screens/QuestionaireComponent.js
+++ components/screens/QuestionaireComponent.js
@@ -14,13 +14,17 @@ class Questionaire extends Component{
             birthDate: "What is your birth date?",
             countryOfOrigin: "What is your country of origin?",
             languageOfPreference: "What is your language of preference?",
+            buttonDate: "Choose a date",
             
             display_name: "",
             email: "",
             country_of_origin: "",
             language_of_preference: "",
-            languages: ['Español', 'English'],
-            countries: ['Dominican Republic', 'Canada', 'United States', 'Afghanistan', 'Åland Islands', 'Albania', 'Algeria', 'American Samoa', 'Andorra', 'Angola', 'Anguilla', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bangladesh', 'Barbados', 'Bahamas', 'Bahrain', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia', 'Bosnia and Herzegovina', 'Botswana', 'Brazil', 'British Indian Ocean Territory', 'British Virgin Islands', 'Brunei Darussalam', 'Bulgaria', 'Burkina Faso', 'Burma', 'Burundi', 'Cambodia', 'Cameroon', 'Cape Verde', 'Cayman Islands', 'Central African Republic', 'Chad', 'Chile', 'China', 'Christmas Island', 'Cocos (Keeling) Islands', 'Colombia', 'Comoros', 'Congo-Brazzaville', 'Congo-Kinshasa', 'Cook Islands', 'Costa Rica', '$_[', 'Croatia', 'Curaçao', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'East Timor', 'Ecuador', 'El Salvador', 'Egypt', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Ethiopia', 'Falkland Islands', 'Faroe Islands', 'Federated States of Micronesia', 'Fiji', 'Finland', 'France', 'French Guiana', 'French Polynesia', 'French Southern Lands', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guadeloupe', 'Guam', 'Guatemala', 'Guernsey', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Heard and McDonald Islands', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati', 'Kuwait', 'Kyrgyzstan', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macau', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Marshall Islands', 'Martinique', 'Mauritania', 'Mauritius', 'Mayotte', 'Mexico', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco', 'Mozambique', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Niue', 'Norfolk Island', 'Northern Mariana Islands', 'Norway', 'Oman', 'Pakistan', 'Palau', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Pitcairn Islands', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'Réunion', 'Romania', 'Russia', 'Rwanda', 'Saint Barthélemy', 'Saint Helena', 'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Martin', 'Saint Pierre and Miquelon', 'Saint Vincent', 'Samoa', 'San Marino', 'São Tomé and Príncipe', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Sint Maarten', 'Slovakia', 'Slovenia', 'Solomon Islands', 'Somalia', 'South Africa', 'South Georgia', 'South Korea', 'Spain', 'Sri Lanka', 'Sudan', 'Suriname', 'Svalbard and Jan Mayen', 'Sweden', 'Swaziland', 'Switzerland', 'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', 'Togo', 'Tokelau', 'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Turks and Caicos Islands', 'Tuvalu', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'Uruguay', 'Uzbekistan', 'Vanuatu', 'Vatican City', 'Vietnam', 'Venezuela', 'Wallis and Futuna', 'Western Sahara', 'Yemen', 'Zambia', 'Zimbabwe']
+            languages: ['Spanish', 'English'],
+            countries: ['Dominican Republic', 'Canada', 'United States', 'Afghanistan', 'Åland Islands', 'Albania', 'Algeria', 'American Samoa', 'Andorra', 'Angola', 'Anguilla', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bangladesh', 'Barbados', 'Bahamas', 'Bahrain', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia', 'Bosnia and Herzegovina', 'Botswana', 'Brazil', 'British Indian Ocean Territory', 'British Virgin Islands', 'Brunei Darussalam', 'Bulgaria', 'Burkina Faso', 'Burma', 'Burundi', 'Cambodia', 'Cameroon', 'Cape Verde', 'Cayman Islands', 'Central African Republic', 'Chad', 'Chile', 'China', 'Christmas Island', 'Cocos (Keeling) Islands', 'Colombia', 'Comoros', 'Congo-Brazzaville', 'Congo-Kinshasa', 'Cook Islands', 'Costa Rica', 'Croatia', 'Curaçao', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'East Timor', 'Ecuador', 'El Salvador', 'Egypt', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Ethiopia', 'Falkland Islands', 'Faroe Islands', 'Federated States of Micronesia', 'Fiji', 'Finland', 'France', 'French Guiana', 'French Polynesia', 'French Southern Lands', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guadeloupe', 'Guam', 'Guatemala', 'Guernsey', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Heard and McDonald Islands', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati', 'Kuwait', 'Kyrgyzstan', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macau', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Marshall Islands', 'Martinique', 'Mauritania', 'Mauritius', 'Mayotte', 'Mexico', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco', 'Mozambique', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Niue', 'Norfolk Island', 'Northern Mariana Islands', 'Norway', 'Oman', 'Pakistan', 'Palau', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Pitcairn Islands', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'Réunion', 'Romania', 'Russia', 'Rwanda', 'Saint Barthélemy', 'Saint Helena', 'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Martin', 'Saint Pierre and Miquelon', 'Saint Vincent', 'Samoa', 'San Marino', 'São Tomé and Príncipe', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Sint Maarten', 'Slovakia', 'Slovenia', 'Solomon Islands', 'Somalia', 'South Africa', 'South Georgia', 'South Korea', 'Spain', 'Sri Lanka', 'Sudan', 'Suriname', 'Svalbard and Jan Mayen', 'Sweden', 'Swaziland', 'Switzerland', 'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', 'Togo', 'Tokelau', 'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Turks and Caicos Islands', 'Tuvalu', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'Uruguay', 'Uzbekistan', 'Vanuatu', 'Vatican City', 'Vietnam', 'Venezuela', 'Wallis and Futuna', 'Western Sahara', 'Yemen', 'Zambia', 'Zimbabwe'],
+        
+            date: new Date(1598051730000),
+            show: false
         }        
     }
 
@@ -36,6 +40,16 @@ class Questionaire extends Component{
         let countryItems = this.state.countries.map( (s, i) => {
             return <Picker.Item key={i} value={s} label={s} />
         });
+
+        const onChange = (event, selectedDate) => {
+            const currentDate = selectedDate || date;
+            this.setState({show: false});
+            this.setState({date: currentDate});
+          };
+
+          const showDatepicker = () => {
+            this.setState({show: true});
+          };
         
         return(
             //TEXT STYLE INCORRECT BUT WORKS!
@@ -43,7 +57,7 @@ class Questionaire extends Component{
                 <View style={styles.formRow}>
                     <Text p style={styles.formLabel}>{this.state.nameQuestion}{'\n'}</Text>
                     <TextInput
-                        style={{ height: 40, width: 200, borderColor: 'gray', borderWidth: 1 }}
+                        style={styles.formItem}
                         onChangeText={(text) => this.setState({display_name: text})}
                         value={this.state.display_name}
                     />
@@ -51,18 +65,29 @@ class Questionaire extends Component{
                 <View style={styles.formRow}>
                     <Text p style={styles.formLabel}>{this.state.emailQuestion}{'\n'}</Text>
                     <TextInput
-                        style={{ height: 40, width: 200, borderColor: 'gray', borderWidth: 1 }}
+                        style={styles.formItem}
                         onChangeText={(text) => this.setState({email: text})}
                         value={this.state.email}
                     />
                 </View>
                 <View style={styles.formRow}>
                     <Text p style={styles.formLabel}>{this.state.birthDate}{'\n'}</Text>
+                    <Button style={ styles.formItem } onPress={showDatepicker} title={this.state.date.toLocaleDateString()}></Button>
+                    {this.state.show && (
+                        <DateTimePicker
+                        testID="dateTimePicker"
+                        value={this.state.date}
+                        mode='date'
+                        is24Hour={true}
+                        display="default"
+                        onChange={onChange}
+                        />
+                    )}
                 </View>
                 <View style={styles.formRow}>
                     <Text p style={styles.formLabel}>{this.state.countryOfOrigin}{'\n'}</Text>
                     <Picker 
-                        style={{ height: 40, width: 200, borderColor: 'gray', borderWidth: 1 }}
+                        style={styles.formItem}
                         selectedValue={this.state.country_of_origin}
                         onValueChange={(itemValue, itemIndex) => this.setState({country_of_origin: itemValue})}>                       
                         {countryItems}
@@ -71,7 +96,7 @@ class Questionaire extends Component{
                 <View style={styles.formRow}>
                     <Text p style={styles.formLabel}>{this.state.languageOfPreference}{'\n'}</Text>
                     <Picker 
-                        style={{ height: 40, width: 200, borderColor: 'gray', borderWidth: 1 }}
+                        style={styles.formItem}
                         selectedValue={this.state.language_of_preference}
                         onValueChange={(itemValue, itemIndex) => this.setState({language_of_preference: itemValue})}>                       
                         {languageItems}
@@ -104,8 +129,11 @@ const styles = StyleSheet.create({
         fontSize: 18,
         flex: 2
     },
-    formItem: {
-        flex: 1
+    formItem: { 
+        height: 40, 
+        width: 200, 
+        borderColor: 'gray', 
+        borderWidth: 1 
     }
   });
 
