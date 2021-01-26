97c502d2d545e2b5ad0221a6b01b04a10603c754
Pgrm97 <Pgrm97@gmail.com>
2021-01-25 22:49:47 -0400

    Intro Form saves to State

--- components/screens/QuestionaireComponent.js
+++ components/screens/QuestionaireComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom';
-import { Image, View, StyleSheet, ScrollView } from 'react-native'
+import { Image, View, StyleSheet, ScrollView, TextInput } from 'react-native'
 import { Text, Button } from 'react-native-elements'
 import UselessTextInput from '../TextInputComponent';
 import DateTimePicker from '@react-native-community/datetimepicker';
@@ -8,15 +8,19 @@ import DateTimePicker from '@react-native-community/datetimepicker';
 class Questionaire extends Component{
     constructor(props){
         super(props);
-
         this.state = {
             nameQuestion: "What is your name?",
             emailQuestion: "What is your email address?",
             birthDate: "What is your birth date?",
             countryOfOrigin: "What is your country of origin?",
-            languageOfPreference: "What is your language of preference?"
-        }
-        
+            languageOfPreference: "What is your language of preference?",
+            
+            display_name: "",
+            email: "",
+            country_of_origin: "",
+            language_of_preference: ""
+
+        }        
     }
 
     render(){       
@@ -24,36 +28,45 @@ class Questionaire extends Component{
             <ScrollView>
                 <View style={styles.formRow}>
                     <Text p style={styles.formLabel}>{this.state.nameQuestion}{'\n'}</Text>
-                    <UselessTextInput/>
+                    <TextInput
+                        style={{ height: 40, width: 200, borderColor: 'gray', borderWidth: 1 }}
+                        onChangeText={(text) => this.setState({display_name: text})}
+                        value={this.state.display_name}
+                    />
                 </View>
                 <View style={styles.formRow}>
                     <Text p style={styles.formLabel}>{this.state.emailQuestion}{'\n'}</Text>
-                    <UselessTextInput/>
+                    <TextInput
+                        style={{ height: 40, width: 200, borderColor: 'gray', borderWidth: 1 }}
+                        onChangeText={(text) => this.setState({email: text})}
+                        value={this.state.email}
+                    />
                 </View>
                 <View style={styles.formRow}>
                     <Text p style={styles.formLabel}>{this.state.birthDate}{'\n'}</Text>
                 </View>
                 <View style={styles.formRow}>
                     <Text p style={styles.formLabel}>{this.state.countryOfOrigin}{'\n'}</Text>
-                    <UselessTextInput/>
+                    <TextInput
+                        style={{ height: 40, width: 200, borderColor: 'gray', borderWidth: 1 }}
+                        onChangeText={(text) => this.setState({country_of_origin: text})}
+                        value={this.state.country_of_origin}
+                    />
                 </View>
                 <View style={styles.formRow}>
                     <Text p style={styles.formLabel}>{this.state.languageOfPreference}{'\n'}</Text>
-                    <UselessTextInput/>
-                </View>
-
-
-
-
-
-                
+                    <TextInput
+                        style={{ height: 40, width: 200, borderColor: 'gray', borderWidth: 1 }}
+                        onChangeText={(text) => this.setState({language_of_preference: text})}
+                        value={this.state.language_of_preference}
+                    />
+                </View>               
                 
                 <Button
                     title="Continue"
                     onPress={ () => this.props.navigation.navigate('Description')}
                     type="solid"
-                />
-                
+                />                
             </ScrollView>
         );
     }
