433c48d736a1099c67ea9f607b17d1cde7839f1b
Pgrm97 <Pgrm97@gmail.com>
2021-01-29 14:51:33 -0400

    Created a places component to test API

--- components/CreatePlacesComponent.js
+++ components/CreatePlacesComponent.js
@@ -1,12 +1,42 @@
 import React, {Component} from 'react'
+import { View, Text } from 'react-native'
 
 class Places extends Component{
     constructor(props){
         super(props);
         this.state = {
-
+            isLoading: true,
+            data: ''
         }
+        this.getMoviesFromApiAsync = this.getMoviesFromApiAsync.bind(this);
     }
 
-    
+    componentDidMount() {
+        this.getMoviesFromApiAsync().then( function(response) {
+            console.log(response);
+        });
+    }
+
+    getMoviesFromApiAsync = async () => {
+        try {
+          let response = await fetch(
+            'https://api.openweathermap.org/data/2.5/onecall?lat=19.790217&lon=-70.690793&exclude=minutely,daily,hourly,alerts&appid=70263032a30d9564a024794abb3ea050'
+          );
+          let json = await response.json();
+          return json;
+        } catch (error) {
+          console.error(error);
+        }
+      };
+
+    render(){
+        return(
+            <View>
+                <Text>{this.state.data}</Text>
+            </View>
+        );
+        
+    }    
 }
+
+export default Places;
--- components/screens/IntroComponent.js
+++ components/screens/IntroComponent.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Image, View, StyleSheet } from 'react-native'
 import { Text, Button } from 'react-native-elements'
 import Database from '../DatabaseComponent'
+import Places from '../CreatePlacesComponent'
 
 function Intro(props) {
 
     return(
         <View style={ styles.container }>
             <Database></Database>
+            <Places></Places>
             <Text style={{marginBottom: 30}} h4>Welcome to Mochileros RD!</Text>
             <Image source={{uri: 'https://cdn0.iconfinder.com/data/icons/tutor-icon-set/512/Backpack_icon-512.png'}}
             style={{width: 150, height: 150, marginBottom: 30 }} />
