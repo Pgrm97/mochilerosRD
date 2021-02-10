256f4293d109deebeee763e631874c0ca7b17072
Pgrm97 <Pgrm97@gmail.com>
2021-02-10 11:52:56 -0400

    Card is populated from DB.

--- components/screens/InitialRatingScreenComponent.js
+++ components/screens/InitialRatingScreenComponent.js
@@ -2,23 +2,24 @@ import React, { Component }from 'react'
 import { View, StyleSheet } from 'react-native'
 import { Card, Text, Rating, AirbnbRating, Button } from 'react-native-elements'
 import RecommendationCard from '../CardComponent';
+import { database } from '../../config';
 
 class InitialRatingScreen extends Component {
 
     constructor(props){
       super(props);
       this.state = {
-        data: ''
+        data: '',
+        context: ''
       }
 
       this.getMoviesFromApiAsync = this.getMoviesFromApiAsync.bind(this);
     }
 
     componentDidMount() {
+      this.accessDBPlaces();
       this.getMoviesFromApiAsync().then( (response) => {
-          alert("Today is a " + this.getDate() + " with " + response.current.weather[0].description + " and the temperature is around " + (response.current.temp - 273.15).toFixed() + "C")
-          //this.setState({data: response});
-          // Temp doesn't have decimals.
+          alert("Today is a " + this.getDate() + " with " + response.current.weather[0].description + " and the temperature is around " + (response.current.temp - 273.15).toFixed() + "C");
       });
   }
 
@@ -28,7 +29,9 @@ class InitialRatingScreen extends Component {
           'https://api.openweathermap.org/data/2.5/onecall?lat=19.790217&lon=-70.690793&exclude=minutely,daily,hourly,alerts&appid=70263032a30d9564a024794abb3ea050'
         );
         let json = await response.json();
-        return json;
+        this.setState({context: json});
+        console.log(json);
+        return this.state.context;
       } catch (error) {
         console.error(error);
       }
@@ -42,15 +45,25 @@ class InitialRatingScreen extends Component {
       return "weekday";
   }
 
+  accessDBPlaces = () => {
+    database.ref('places').once('value').then((snapshot) => {
+        this.setState(
+          {
+            data: snapshot.child('playa-dorada').val()
+          });
+    })
+  }
+
     render(){
         return(
           <View style={ styles.container }>
               <Text h4>How would you rate?</Text>
               <RecommendationCard 
-                  title="Playa Grande"
+                  title={this.state.data.name}
                   context="Sunny ☀️ Weekend"
                   in="in"
-                  image_url="https://lh3.googleusercontent.com/p/AF1QipMi4kaK-EnSEEVCgAK3yaIBeEl05hQp5ebZOJhI=s1600-w800"
+                  image_url={this.state.data.img_url}
+                  directions={"Located in " + this.state.data.address}
                   />
               <AirbnbRating 
               defaultRating={0}/>
