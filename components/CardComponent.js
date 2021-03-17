41d0a5682c8ad572a8c9884dee85c0ee7815d852
Pgrm97 <Pgrm97@gmail.com>
2021-03-17 11:32:25 -0400

    Changed Card Component to accept maps

--- components/CardComponent.js
+++ components/CardComponent.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
 import { Card, Text, Rating, AirbnbRating, Button } from 'react-native-elements'
+import * as Linking from 'expo-linking'
+
+var openMaps = (url) => {
+  Linking.openURL(url);
+};
 
 function RecommendationCard(props) {
 
@@ -18,6 +23,13 @@ function RecommendationCard(props) {
                 <Text style={{marginBottom: 10}}>
                   {props.directions}
                 </Text>
+                {props.lat && 
+                  <Button
+                    title="Google Maps"
+                    onPress={ () => openMaps("https://www.google.com/maps/search/?api=1&query=" + props.lat + "," + props.lng + "&query_place_id=" + props.maps_place_id) }
+                    type="solid"
+                  />
+                }
             </Card>        
     );
 }
