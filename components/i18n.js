33163abf628adc3ec46b02844910ceaa3f89ee60
Pgrm97 <Pgrm97@gmail.com>
2021-03-27 10:29:55 -0400

    Locale starts in EN

--- components/i18n.js
+++ components/i18n.js
@@ -1,13 +1,14 @@
 import I18n from "i18n-js";
-import * as RNLocalize from "react-native-localize";
+//import * as RNLocalize from "react-native-localize";
 
 import en from "../locales/en";
 import es from "../locales/es";
 
-const locales = RNLocalize.getLocales();
+//const locales = RNLocalize.getLocales();
+var locales = ['en', 'es'];
 
 if (Array.isArray(locales)) {
-  I18n.locale = locales[0].languageTag;
+  I18n.locale = locales[0];
 }
 
 I18n.fallbacks = true;
